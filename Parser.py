275757d85195b4bf54d078d911c20f81e3c1a31c
Sammer <samipuran@gmail.com>
2016-05-09 20:55:48 +0200

    added parsing for validation and for getting the filenames in directory for comparison with validation set

--- Parser.py
+++ Parser.py
@@ -1,30 +1,59 @@
 import glob
+import os
+from os.path import basename
+
 class Parser(object):
 
 
-    def __init__(self, directory):
+    def __init__(self, directory,validation):
         self.directory = directory
+        self.validationFile = validation
 
-    def parseFilesInDirectory(self):
+    def get_filename_without_extension(self, path):
+        base = os.path.basename(path)
+        filename = os.path.splitext(base)[0]
+        return filename
+
+
+    def get_filenames_without_extension(self):
         paths = glob.glob(self.directory)
-        parsedFile = map(self.parseFile,paths)
-        return parsedFile
+        filenames = map(self.get_filename_without_extension, paths)
+        print(*filenames, sep='\n')
 
-    def parseFile(self,path):
+    def parse_validation_file(self):
+        validation_lines = [line.rstrip('\n') for line in open(self.validationFile)]
+        parsed_validation = map(self.parse_validation_line, validation_lines)
+        return parsed_validation
+
+
+
+    def parse_validation_line(self,validation_line):
+        validation_vector = validation_line.split()
+        filename_abbreviated = validation_vector[0]
+        filename = filename_abbreviated[:4] + 'g-' + filename_abbreviated[4:]
+        genuine_or_fake = validation_vector[1]=='g'
+        return filename, genuine_or_fake
+
+    def parse_files_in_directory(self):
+        paths = glob.glob(self.directory)
+        parsed_file = map(self.parse_file, paths)
+        return parsed_file
+
+    def parse_file(self, path):
         lines = [line.rstrip('\n') for line in open(path)]
-        measurements = map(self.parseFileToFeature, lines)
+        measurements = map(self.parse_line_to_measurements, lines)
         return measurements
 
-    def parseFileToFeature(self, line):
-
+    def parse_line_to_measurements(self, line):
         desired_array = [float(numeric_string) for numeric_string in line.split()]
         return desired_array
 
     def main(self):
-        lists = self.parseFilesInDirectory()
+        lists = self.parse_files_in_directory()
         for listFeature in lists:
             for feature in listFeature:
                 print(feature)
 
 
-if __name__ == "__main__": Parser("/home/sammer/signature_verification/Signature-Verification/enrollment/*.txt").main()
+
+if __name__ == "__main__": Parser("/home/sammer/signature_verification/Signature-Verification/enrollment/*.txt","/home/sammer/signature_verification/Signature-Verification/verification-gt.txt").main()
