d0b2b7a1385dc0730c0b1b6ea6f939c66d425b7e
masus04 <thomas_st@hotmail.com>
2016-05-14 13:37:50 +0200

    minor changes

--- main.py
+++ main.py
@@ -21,16 +21,25 @@ def print_timer(purpose_message=""):
     start_time = time.clock()
 
 """ Reading Data """
-enrollment = features.calculate_features(Parser.parse_files_in_directory(enrollment_path))
-verification = features.calculate_features(Parser.parse_files_in_directory(verification_path))
+def sort_by_name(signature):
+    return signature.filename
+
+enrollment = sorted(features.calculate_features(Parser.parse_files_in_directory(enrollment_path)), key = sort_by_name)
+verification = sorted(features.calculate_features(Parser.parse_files_in_directory(verification_path)), key = sort_by_name)
 verification_gt = Parser.parse_validation_file(verification_gt_path)
 
+print("enrollment names:")
+for enr in enrollment[:5]:
+    print(enr.filename)
 
+print("verification names:")
+for ver in verification[:5]:
+    print(ver.filename)
 
+print("verification-gt")
+print(verification_gt)
 
 for template in verification:
     dtw = DTW(template)
 
     results = [dtw.calculate_cost_and_matrix(enr) for enr in enrollment]
-
-
