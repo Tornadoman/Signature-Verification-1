13f624ce0cf4073adecf35fcd0d0c5c6d96c62ac
masus04 <thomas_st@hotmail.com>
2016-05-15 16:19:23 +0200

    minor skeleton changes

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ from config import verification_path
 # timer
 start_time = time.clock()
 
+""" Helper Functions"""
+
 
 def print_timer(purpose_message=""):
     global start_time
@@ -24,22 +26,31 @@ def sort_by_name(signature):
     return signature.filename
 
 
-enrollment = sorted(features.calculate_features(Parser.parse_files_in_directory(enrollment_path)), key=sort_by_name)
-verification = sorted(features.calculate_features(Parser.parse_files_in_directory(verification_path)), key=sort_by_name)
-verification_gt = Parser.parse_validation_file(verification_gt_path)
-
-print("enrollment names:")
-for enr in enrollment[:5]:
-    print(enr.filename)
-
-print("verification names:")
-for ver in verification[:5]:
-    print(ver.filename)
-
-print("verification-gt")
-print(verification_gt)
-
-for template in verification:
+def apply_dtw(template):
     dtw = DTW(template)
 
     results = [dtw.calculate_cost_and_matrix(enr) for enr in enrollment]
+    min_cost = sorted(results)[0]
+    print("results for template %s complete. Top result: %s" % (template.filename, max))
+    template.cost = min_cost[0]
+
+
+""" Parsing """
+
+
+enrollment = sorted(features.calculate_features(Parser.parse_files_in_directory(enrollment_path)), key=sort_by_name)
+verification = sorted(features.calculate_features(Parser.parse_files_in_directory(verification_path)), key=sort_by_name)
+verification_gt = Parser.parse_validation_file(verification_gt_path)
+print_timer("parsing")
+
+
+""" Applying DTW """
+
+# adjust verification set size here
+for template in verification[:10]:
+    apply_dtw(template)
+
+print_timer("DTW")
+
+for template in verification[:10]:
+    print("%s, cost: %s" %(template.filename, template.cost))
